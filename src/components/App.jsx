167633ddb57d5c005f3a5f89147429639c7c8650
Alina Proskurnitska <proskur1987@gmail.com>
2022-10-18 15:36:21 +0300

    add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,7 @@ import { AppStyled } from './App.styled';
 
 export class App extends Component {
     state = {
-        contacts: [
-            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-        ],
+        contacts: [ ],
         filter: '',
     };
 
@@ -24,13 +19,13 @@ export class App extends Component {
         alreadyAdded
             ? alert(`${currentValue.name} is already in contacts`)
             : this.setState(prevState => {
-                  const newContact = {
-                      name: currentValue.name,
-                      id: nanoid(),
-                      number: currentValue.number,
-                  };
-                  return { contacts: [...prevState.contacts, newContact] };
-              });
+                const newContact = {
+                    name: currentValue.name,
+                    id: nanoid(),
+                    number: currentValue.number,
+                };
+                return { contacts: [...prevState.contacts, newContact] };
+            });
     };
 
     deleteContact = contactId => {
@@ -45,7 +40,29 @@ export class App extends Component {
         this.setState({ filter: e.target.value });
     };
 
+    componentDidMount() {
+        console.log('App componentDidMount')
+
+        const contacts = localStorage.getItem('contacts');
+        const parsedContacts = JSON.parse(contacts);
+
+        if (parsedContacts) {
+            this.setState({ contacts: parsedContacts });
+        }
+    }
+
+    componentDidUpdate(preveProps, prevState) {
+        console.log('App componentDidUpdate')
+
+        if (this.state.contacts !== prevState.contacts) {
+            console.log('оновлення contacts');
+
+            localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
+        }
+    };
+
     render() {
+        console.log('App render')
         const normalizedFilter = this.state.filter.toLowerCase();
         const filtredContacts = this.state.contacts.filter(contact =>
             contact.name.toLowerCase().includes(normalizedFilter)
